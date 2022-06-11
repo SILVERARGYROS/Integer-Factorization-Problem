a0abc8e7d21db8193bc4434e531e975e732d464f
SILVERARGYROS <76134085+SILVERARGYROS@users.noreply.github.com>
2022-06-11 20:20:33 +0300

    basic algorithm illustration
    first algorithm idea.
    works for any positive integers up to 2.147.483.647 (integer variable limit)

--- ifp.c
+++ ifp.c
@@ -2,8 +2,48 @@
 
 int main (void)
 {
+    int i;              // loop variable
+    int n;              // input
+    int remaining;      // helping variable  
+    int primeInput;     // check for prime number input
+
+    //receiving input
+    printf("Please enter a composite positive number\n");
+    scanf("%d", &n);
 
 
+    //initializations
+    remaining = n;
+    primeInput = 1;
 
-    return n;
+    printf("debug n == %d\n", n);
+    //looking for factors
+    for(i = 2; i < remaining; i++)
+    {
+        // printf("debug i == %d\n", i);
+        if(remaining % i == 0)
+        {
+            if(primeInput)
+            {
+                primeInput = 0;
+                printf("%d == ", n);
+            }
+
+            remaining = remaining / i;
+            printf("%d * ", i);
+            i = 1;
+            // printf("debug found dividor so i == %d\n", i);
+        }
+    }
+
+    if(primeInput)
+    {
+        printf("The number you gave is prime\n");
+    }
+    else
+    {
+        printf("%d\n", remaining);
+    }
+
+    return 0;
 }
